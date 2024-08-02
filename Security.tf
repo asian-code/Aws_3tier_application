cbee365fd8871305efc992a1a6c25982382e69fc
EricVM <123rex100@gmail.com>
2024-08-02 09:39:12 -0400

    cleaned up modules and need to test

--- Security.tf
+++ Security.tf
@@ -1,66 +1,80 @@
 # for security
-resource "aws_security_group" "web_sg" {
-  vpc_id = aws_vpc.main.id
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
+# Public SG ---
+resource "aws_security_group" "public_sg" {
+  name        = "public-sg-tf"
+  description = "inbound only to p:80,443. outbound to all"
+  vpc_id      = aws_vpc.main.id
   tags = {
-    Name = "web_sg"
+    Name = "public_sg"
   }
 }
-
+resource "aws_vpc_security_group_egress_rule" "public_allow_all_ipv4" {
+  security_group_id = aws_security_group.public_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+resource "aws_vpc_security_group_ingress_rule" "public_allow_http_ipv4" {
+  security_group_id = aws_security_group.public_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+resource "aws_vpc_security_group_ingress_rule" "public_allow_tls_ipv4" {
+  security_group_id = aws_security_group.public_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+# App SG ---
 resource "aws_security_group" "app_sg" {
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.1.0/24"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
+  name        = "app-sg-tf"
+  description = "allow inbound from public_sg and outbound to db_sg,public_sg "
+  vpc_id      = aws_vpc.main.id
   tags = {
     Name = "app_sg"
   }
 }
+resource "aws_vpc_security_group_egress_rule" "app_allow_all_dbsg" {
+  security_group_id            = aws_security_group.app_sg.id
+  cidr_ipv4                    = "0.0.0.0/0"
+  ip_protocol                  = "-1" # semantically equivalent to all ports
+  referenced_security_group_id = aws_security_group.db_sg.id
+}
+resource "aws_vpc_security_group_egress_rule" "app_allow_all_publicsg" {
+  security_group_id            = aws_security_group.app_sg.id
+  cidr_ipv4                    = "0.0.0.0/0"
+  ip_protocol                  = "-1" # semantically equivalent to all ports
+  referenced_security_group_id = aws_security_group.public_sg.id
+}
+resource "aws_vpc_security_group_ingress_rule" "app_allow_from_public" {
+  security_group_id            = aws_security_group.app_sg.id
+  cidr_ipv4                    = "0.0.0.0/0"
+  ip_protocol                  = "-1"
+  referenced_security_group_id = aws_security_group.public_sg.id
 
+}
+# db SG ---
 resource "aws_security_group" "db_sg" {
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.2.0/24"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
+  name        = "db-sg-tf"
+  description = "For resources in the public subnet"
+  vpc_id      = aws_vpc.main.id
   tags = {
     Name = "db_sg"
   }
+}
+resource "aws_vpc_security_group_ingress_rule" "db_allow_from_app" {
+  security_group_id            = aws_security_group.db_sg.id
+  cidr_ipv4                    = "0.0.0.0/0"
+  ip_protocol                  = "-1"
+  referenced_security_group_id = aws_security_group.app_sg.id
+
+}
+resource "aws_vpc_security_group_egress_rule" "db_allow_all_to_app" {
+  security_group_id            = aws_security_group.db_sg.id
+  cidr_ipv4                    = "0.0.0.0/0"
+  ip_protocol                  = "-1" # semantically equivalent to all ports
+  referenced_security_group_id = aws_security_group.app_sg.id
 }
--- app.tf
+++ app.tf
@@ -1,72 +1,28 @@
 # for main componets of all tiers
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 resource "aws_instance" "web" {
-  ami           = "ami-0c55b159cbfafe1f0" # Replace with your preferred AMI
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public.id
-  security_groups = [aws_security_group.web_sg.name]
+  security_groups = [aws_security_group.public_sg.name]
 
   tags = {
-    Name = "web_instance"
-  }
-}
-
-resource "aws_instance" "app" {
-  ami           = "ami-0c55b159cbfafe1f0" # Replace with your preferred AMI
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.private.id
-  security_groups = [aws_security_group.app_sg.name]
-
-  tags = {
-    Name = "app_instance"
-  }
-}
-
-resource "aws_elb" "web" {
-  name               = "web_elb"
-  availability_zones = ["us-west-2a"]
-
-  listener {
-    instance_port     = 80
-    instance_protocol = "HTTP"
-    lb_port           = 80
-    lb_protocol       = "HTTP"
-  }
-
-  instances = [aws_instance.web.id]
-
-  health_check {
-    target              = "HTTP:80/"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
-
-  tags = {
-    Name = "web_elb"
-  }
-}
-# database
-resource "aws_db_subnet_group" "default" {
-  name       = "main_subnet_group"
-  subnet_ids = [aws_subnet.db.id]
-
-  tags = {
-    Name = "main_subnet_group"
-  }
-}
-
-resource "aws_db_instance" "default" {
-  allocated_storage    = 20
-  engine               = "mysql"
-  instance_class       = "db.t2.micro"
-  # name                 = "mydb"
-  username             = "admin"
-  password             = var.db_password
-  db_subnet_group_name = aws_db_subnet_group.default.name
-  vpc_security_group_ids = [aws_security_group.db_sg.id]
-
-  tags = {
-    Name = "main_db_instance"
+    Name = "test web_instance"
   }
 }
